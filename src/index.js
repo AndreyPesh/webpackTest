26ef6f794605ee7b4b35ac5857a2fb9507bea871
Andrey Peshko <coursestud@mail.ru>
2021-11-26 19:05:22 +0300

    feat: implement example carry function

--- src/index.js
+++ src/index.js
@@ -1,43 +1,21 @@
-const person = {
-  name: 'Andy',
+function sum(a, b, c) {
+  return a + b + c
 }
 
-function info(phone, email) {
-  console.log('Name', this.name)
-  console.log('Phone', phone)
-  console.log('Email', email)
-  return ''
-}
-
-function bind(func, context, ...args) {
-  const fn = func
-  const uniqueKey = new Date()
-  return function(...rest) {
-    context[uniqueKey] = fn
-    const result = context[uniqueKey](...args.concat(rest))
-    delete context[uniqueKey]
-    return result
+function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(func, args)
+    } else {
+      return function(...rest) {
+        return curried.apply(func, args.concat(rest))
+      }
+    }
   }
 }
 
-Object.prototype.myBind1 = function(context, ...args) {
-  const currentContext = this
-  return function(...rest) {
-    return currentContext.apply(context, args.concat(rest))
-  }
-}
+const curried = curry(sum)
 
-Object.prototype.myBind2 = function(context, ...args) {
-  const currentContext = this
-  return function(...rest) {
-    const uniqueKey = new Date()
-    context[uniqueKey] = currentContext
-    const result = context[uniqueKey](...args.concat(rest))
-    delete context[uniqueKey]
-    return result
-  }
-}
-
-info.myBind1(person, '2039490375')( 'mail')
-const binding = info.myBind2(person)
-binding('123123', 'newmail')
+console.log(sum(1, 2, 3))
+console.log(curried(1, 2)(3))
+console.log(curried(1)(2, 3))
