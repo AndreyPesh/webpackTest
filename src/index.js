0e4fdf094193b5dd883050d376103f6146b25353
Andrey Peshko <coursestud@mail.ru>
2021-11-26 18:15:47 +0300

    feat: implement custom bind

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import './style/style.scss'
-import imageSrc from './assets/bg.webp'
-const body = document.body
-import fun from './fun.ts'
+const person = {
+  name: 'Andy',
+}
 
-const img = new Image ()
-img.src = imageSrc
-body.append(img)
+function info(phone, email) {
+  console.log('Name', this.name)
+  console.log('Phone', phone)
+  console.log('Email', email)
+  return ''
+}
 
-console.log('without loaders');
+function bind(func, context, ...args) {
+  const fn = func
+  const uniqueKey = new Date()
+  return function(...rest) {
+    context[uniqueKey] = fn
+    const result = context[uniqueKey](...args.concat(rest))
+    delete context[uniqueKey]
+    return result
+  }
+}
 
-console.log(fun());
+Object.prototype.myBind1 = function(context, ...args) {
+  const currentContext = this
+  return function(...rest) {
+    return currentContext.apply(context, args.concat(rest))
+  }
+}
+
+Object.prototype.myBind2 = function(context, ...args) {
+  const currentContext = this
+  return function(...rest) {
+    const uniqueKey = new Date()
+    context[uniqueKey] = currentContext
+    const result = context[uniqueKey](...args.concat(rest))
+    delete context[uniqueKey]
+    return result
+  }
+}
+
+info.myBind1(person, '2039490375')( 'mail')
+const binding = info.myBind2(person)
+binding('123123', 'newmail')
